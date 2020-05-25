6d2d0587c16ec0cf669a725a208e892e94d0e7cb
MyNa Do <myna.do@mahoyo.com>
2020-05-25 18:22:08 +0200

    boilerplate nodejs api

---
+++ routes/index.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const router = express.Router();
+
+/* GET / */
+router.get('/', (req, res) => {
+	res.send({
+		status: 'success'
+	});
+});
+
+module.exports = router;
